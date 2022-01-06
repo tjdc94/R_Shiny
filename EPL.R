7bc4206ed58750f673d0eee9ef5591f1f7bfe6f2
tjdc94 <tjd.cole@gmail.com>
2022-01-06 09:05:15 +0000

    Update EPL.R

--- EPL.R
+++ EPL.R
@@ -1,7 +1,7 @@
 getwd()
 setwd("/Users/tjdcole/Documents/GitHub/R_Shiny")
 getwd()
-epl <- read.csv("EPL_20_21.csv")
+epl <- read.csv("EPL_20_21.csv", stringsAsFactors=TRUE)
 head(epl)
 summary(epl)
 str(epl)
@@ -12,7 +12,14 @@ library("ggplot2")
 library("dplyr")
 
 xg_xa <- epl %>%
-  select(Name, xG, xA)
+  select(Name, xG, Position, xA, Club) %>%
+  filter(Position != "GK") %>%
+  separate(col = Position, c("Position1", "Position2"), extra = "drop", fill = "right")
 head(xg_xa)
+str(xg_xa)
+summary(xg_xa)
 
-ggplot(data=)
+p <- ggplot(data= xg_xa %>%
+         select(Name, xG, xA),aes(x=xG, y=xA)) 
+
+p + geom_point(colour = "red", alpha = 0.5)
